4dd0d3d7ea0054d9b56505a6166e59195c561db8
ckiahtipes <chris.kiahtipes@gmail.com>
2023-11-03 11:23:01 -0400

    Initiating mapping Bantu repository.

---
+++ mapping_bantu.R
@@ -0,0 +1,17 @@
+#Mapping Bantu script
+
+#Libraries
+
+library(raster)
+library(terra)
+library(lipdR)
+library(neotoma2)
+
+#Start with locations of cores, extract climate and vegetation data.
+
+#1) Needs a list of records from Neotoma.
+#2) Needs a list of records from APD
+
+
+
+#Extract chronological data from each record.
